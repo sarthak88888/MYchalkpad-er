153471cdfcaf245c1a004f53c36e72b49f259743
sarthak88888 <sarthakpundir0@gmail.com>
2026-03-07 16:03:46 +0530

    Fix icons, splash, babel config, app.json

--- babel.config.js
+++ babel.config.js
@@ -2,24 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
   };
 };
-```
-
----
-
-### `firestore.rules`
-```
-rules_version = '2';
-service cloud.firestore {
-  match /databases/{database}/documents {
-    match /users/{phone} {
-      allow read, write: if request.auth != null
-        && request.auth.token.phone_number == ('+91' + phone);
-    }
-    match /schools/{schoolId}/{document=**} {
-      allow read: if request.auth != null;
-      allow write: if request.auth != null;
-    }
-  }
-}
