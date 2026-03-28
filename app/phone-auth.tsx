7fe89f11d84bb57fff36eb13ff0c31d51922f2df
Sarthak Pundir <sarthakpundir0@gmail.com>
2026-03-28 23:02:58 +0530

    Update phone-auth.tsx

--- app/phone-auth.tsx
+++ app/phone-auth.tsx
@@ -1,4 +1,4 @@
-]import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
   View, Text, StyleSheet, TextInput, TouchableOpacity,
   ActivityIndicator, Alert, KeyboardAvoidingView, Platform,
